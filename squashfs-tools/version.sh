cde45944a7967de0f0003d7a0c4f405cdbc65954
Alexander Couzens <lynxis@fe80.eu>
2017-11-01 17:44:36 +0100

    try_version: use source_date_epoch as fallbackg

--- squashfs-tools/version.sh
+++ squashfs-tools/version.sh
@@ -11,7 +11,7 @@ export TZ=UTC
 TOP="$1"
 OUTPUT="$2"
 REV=""
-SOURCE_DATE_EPOCH=""
+_SOURCE_DATE_EPOCH=""
 
 if [ "$#" -gt 2 ] ; then
 	echo "$0 [[<topdir>] <outputfile>]"
@@ -38,23 +38,30 @@ esac
 try_version() {
         [ -f version ] && [ -f version.date ] || return 1
         REV="$(cat version)"
-        SOURCE_DATE_EPOCH="$(cat version.date)"
+        _SOURCE_DATE_EPOCH="$(cat version.date)"
 
-        [ -n "$REV" ] && [ -n "$SOURCE_DATE_EPOCH" ]
+        [ -n "$REV" ] && [ -n "$_SOURCE_DATE_EPOCH" ]
 }
 
 try_git() {
 	[ -d .git ] || return 1
 
 	REV="$(git describe --tags 2>/dev/null)"
-	SOURCE_DATE_EPOCH="$(git log -1 --format=format:%ct)"
+	_SOURCE_DATE_EPOCH="$(git log -1 --format=format:%ct)"
 
-        [ -n "$REV" ] && [ -n "$SOURCE_DATE_EPOCH" ]
+        [ -n "$REV" ] && [ -n "$_SOURCE_DATE_EPOCH" ]
+}
+
+try_source_date_epoch() {
+    # tries to use the environment
+    if [ -n "$SOURCE_DATE_EPOCH" ] ; then
+        _SOURCE_DATE_EPOCH="$SOURCE_DATE_EPOCH"
+    fi
 }
 
 output_version() {
 	echo "Writing $OUTPUT"
-	DATE="$(our_date "$SOURCE_DATE_EPOCH" +%Y/%m/%d)"
+	DATE="$(our_date "$_SOURCE_DATE_EPOCH" +%Y/%m/%d)"
 	cat > "$OUTPUT" <<EOF
 #define VERSION_STR "squashfskit-$REV"
 #define VERSION_DATE_STR "$DATE"
@@ -62,5 +69,5 @@ EOF
 }
 
 cd "$TOP"
-try_git || try_version || REV="unkown"
+try_git || try_version || try_source_date_epoch || _SOURCE_DATE_EPOCH="2"
 output_version
