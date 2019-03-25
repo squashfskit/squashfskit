refi64/unappimage

commit 97537a0360437c091b092a63c91d4147ce31b7c8

flag_deflator: initialize the compressor()
Some compressor (e.g. lzo) requires to be initialized.
This was broken since afc0c76a170b.

This patch was done by Chris Lamb <lamby@debian.org>
Fixes: afc0c76a170b ("remove frag_deflator_thread")

squashfs-tools/mksquashfs.c
@@ -2406,11 +2406,14 @@ void *deflator(void *arg)
 
 void frag_deflator(struct file_buffer *file_buffer)
 {
+	int res, c_byte, compressed_size;
+	struct file_buffer *write_buffer;
 
-	int c_byte, compressed_size;
-	struct file_buffer *write_buffer =
-		cache_get(fwriter_buffer, file_buffer->block);
+	res = compressor_init(comp, &stream, block_size, 1);
+	if(res)
+		BAD_ERROR("frag_deflator:: compressor_init failed\n");
 
+	write_buffer = cache_get(fwriter_buffer, file_buffer->block);
 	c_byte = mangle2(stream, write_buffer->data, file_buffer->data,
 			 file_buffer->size, block_size, noF, 1);
 	compressed_size = SQUASHFS_COMPRESSED_SIZE_BLOCK(c_byte);

squashfskit/squashfskit

commit 7c79839492fd68fe3fdc82320b85c681731f23c2

mksquashfs: fix compressor initialisation in frag_deflator
Since 97537a036043 the compressor will be initialized on every call
introducing a memory leak on every frag_deflator called.
Use a static and only initialize the stream only once, will leak only
once, as all comp_init() may leak.

Fixes: 97537a036043 ("flag_deflator: initialize the compressor()")
Signed-off-by: Alexander Couzens <lynxis@fe80.eu>

squashfs-tools/mksquashfs.c
@@ -2409,12 +2409,18 @@ void frag_deflator(struct file_buffer *file_buffer)
 	int res, c_byte, compressed_size;
 	struct file_buffer *write_buffer;
 
-	res = compressor_init(comp, &stream, block_size, 1);
-	if(res)
-		BAD_ERROR("frag_deflator:: compressor_init failed\n");
+	static int frag_deflator_comp_init = FALSE;
+	static void *frag_stream = NULL;
+
+	if (frag_deflator_comp_init == FALSE) {
+		frag_deflator_comp_init = TRUE;
+		res = compressor_init(comp, &frag_stream, block_size, 1);
+		if(res)
+			BAD_ERROR("frag_deflator:: compressor_init failed\n");
+	}
 
 	write_buffer = cache_get(fwriter_buffer, file_buffer->block);
-	c_byte = mangle2(stream, write_buffer->data, file_buffer->data,
+	c_byte = mangle2(frag_stream, write_buffer->data, file_buffer->data,
 			 file_buffer->size, block_size, noF, 1);
 	compressed_size = SQUASHFS_COMPRESSED_SIZE_BLOCK(c_byte);
 	write_buffer->size = compressed_size;